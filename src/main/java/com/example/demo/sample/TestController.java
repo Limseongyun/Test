5a948e63ec42caff79043a0bf86fd6b52df2fecb
seongyun.lim <seongyun.lim@e4net.net>
2021-10-25 16:01:50 +0900

    set loglevel debug

--- src/main/java/com/example/demo/sample/TestController.java
+++ src/main/java/com/example/demo/sample/TestController.java
@@ -1,14 +1,15 @@
-package com.example.demo;
-
-import org.springframework.stereotype.Controller;
+package com.example.demo.sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class TestController {
 
 	@GetMapping("/test")
 	public String test() {
-		return "test";
+		log.debug("test");
+		return "test123";
 	}
 }
---
+++ src/test/java/others/MapTest.java
@@ -0,0 +1,11 @@
+package others;
+
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+
+	@Test
+	public void test1() {
+		System.out.print("test");
+	}
+}
