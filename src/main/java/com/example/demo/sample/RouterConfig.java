83657f7be10685bcef25ba27e1875b92ffea2483
seongyun.lim <seongyun.lim@e4net.net>
2021-10-29 21:41:51 +0900

    thymeaf test

--- src/main/java/com/example/demo/sample/RouterConfig.java
+++ src/main/java/com/example/demo/sample/RouterConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.sample;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,6 +14,13 @@ public class RouterConfig {
 	public RouterFunction<ServerResponse> routerExamle(SampleHandler sam){
 		return RouterFunctions.route()
 				.GET("/sample/{id}", sam::test)
+				.GET("/thymeleaf/{}", sam::thymeleaf)
 				.build();
 	}
+	
+	
+	RouterFunction<ServerResponse> route = RouterFunctions.route()
+		    .GET("/hello-world", request -> ServerResponse.ok().bodyValue("Hello World")).build();
+
+
 }
--- src/main/java/com/example/demo/sample/SampleHandler.java
+++ src/main/java/com/example/demo/sample/SampleHandler.java
@@ -1,5 +1,8 @@
 package com.example.demo.sample;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -15,5 +18,10 @@ public class SampleHandler {
 	public Mono<ServerResponse> test(ServerRequest request){
 		log.debug("test handler");
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(request.pathVariable("id"));
+	}
+	
+	public Mono<ServerResponse> thymeleaf(ServerRequest req){
+		Map<String, Object> model = new HashMap<>();
+		return ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("index2", model);
 	}
 }
